01ed3bb3dfc2f9159887dc62aa0919673640ba20
Jacqueline Ta <hello@techiejackie.com>
2023-01-02 11:52:16 -0700

    Default project with basic fetch

---
+++ src/components/CitySearch.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+div
+    h1 Welcome!
+    button(@click="getData()") Click Me
+    section
+        div {{ result }}
+
+</template>
+
+<script lang="ts">
+const url = 'https://api.waqi.info/feed/paris/?token=107e07356961086733ad81e14fb9e6a344aa7562'
+export default {
+    data() {
+        return {
+            result: null,
+        }
+    },
+    methods: {
+        async getData() {
+            fetch(url)
+            .then(response => response.json())
+            .then(json => {
+                if (json.status === 'ok') {
+                    this.result = json.data;
+                }             
+            })
+        }
+    }
+}
+</script>
